7e3b295954c7639293e5520ec1fea96f98156ec4
Artur Skowronski <a.skowronski@smartrecruiters.com>
2016-01-08 01:35:31 +0100

    Support for code in Team request

--- src/hiddencitygame_clj/routes/team.clj
+++ src/hiddencitygame_clj/routes/team.clj
@@ -2,37 +2,49 @@
   (:require [hiddencitygame-clj.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+  )
   )
 
 (defroutes register-team-routes
    (POST "/registerTeam" []
      (fn [req]
        (let [
-             data (get (:params req) "data")
+             gcm (get (:params req) "gcm")
             ]
          {
           :status  200
           :headers {"Content-Type" "application/json"}
-          :body    {:foo (str "registerTeam:" data)}
+          :body    {:foo (str "registerTeam:" gcm)}
           }
        )
      )
    )
 )
 
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :gcm v/required
+    )
+  )
+)
+
 (defroutes join-team-routes
  (POST "/joinTeam" []
    (fn [req]
      (let [
-           data (get (:params req) "data")
-           ]
+           gcm (get (:params req) "gcm")
+           code (get (:params req) "code")
+          ]
        {
         :status  200
         :headers {"Content-Type" "application/json"}
-        :body    {:foo (str "joinTeam:" data)}
-        }
-       )
+        :body    {:foo (str "joinTeam:" gcm ":" code)}
+        })
      )
    )
  )
