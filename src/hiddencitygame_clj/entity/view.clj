cbd690f5bf05c797efc4795f3e072e3e97cf273a
Artur Skowronski <a.skowronski@smartrecruiters.com>
2016-01-10 18:47:24 +0100

    Moving Handlebars to it's own namespace

--- src/hiddencitygame_clj/entity/view.clj
+++ src/hiddencitygame_clj/entity/view.clj
@@ -1,27 +1,10 @@
 (ns hiddencitygame-clj.entity.view
-  (:require [clojure.java.io :as io])
-  (:import (com.github.jknack.handlebars Handlebars Template)
-))
-
-
-(defn read-template
-  "Read HBS Template"
-  [name]
-  (slurp
-    (io/file
-    (io/resource
-      (str "hbs/" name ".hbs")))
-    )
-  )
+  (:require [clojure.java.io :as io]
+            [hiddencitygame-clj.handlebars :as hbs])
+)
 
 (defn render-view
-  "Stub Method"
+  "Render View Method"
   [data]
-  (let [hbs (Handlebars.)]
-   (->
-     hbs
-     (.compileInline (read-template (get data :tpl)))
-     (.apply (get data :params))
-   )
-  )
+  (hbs/render-view data)
 )
---
+++ src/hiddencitygame_clj/handlebars.clj
@@ -0,0 +1,26 @@
+(ns hiddencitygame-clj.handlebars
+  (:require [clojure.java.io :as io])
+  (:import (com.github.jknack.handlebars Handlebars Template))
+  )
+
+(defn read-template
+  "Read HBS Template"
+  [name]
+  (slurp
+    (io/file
+      (io/resource
+        (str "hbs/" name ".hbs")))
+    )
+  )
+
+(defn render-view
+  "Stub Method"
+  [data]
+  (let [hbs (Handlebars.)]
+    (->
+      hbs
+      (.compileInline (read-template (get data :tpl)))
+      (.apply (get data :params))
+      )
+    )
+  )
